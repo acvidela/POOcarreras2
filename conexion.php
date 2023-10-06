a745023bca504fd328588887b19343ace93c3619
acvidela <acvidela@gmail.com>
2023-10-05 21:03:51 -0300

    conexion separando los parámetros, no con URL, se conectó

--- conexion.php
+++ conexion.php
@@ -1,25 +1,31 @@
 <?php
     class Conexion{
         static $db = null;
+        private $host = 'mahmud.db.elephantsql.com';
+        private $port = '5432';
+        private $database = 'htslmwoa';
+        private $user = 'htslmwoa';
+        private $password = 'ZRmocbFztc6uzg9rVaWzB11E-jgPEFT6';
     
-    private function __construct(){
-    try {
-        $url = "postgres://htslmwoa:ZRmocbFztc6uzg9rVaWzB11E-jgPEFT6@mahmud.db.elephantsql.com/htslmwoa";
-    
-        // Crear una instancia de PDO con la URL de conexión
-        self::$db = new PDO($url);
-    
-        // Configurar el modo de error de PDO para manejar excepciones
-        self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-        // Puedes usar esta conexión para realizar consultas
-        return self::$db;
-    } catch (PDOException $e) {
-        // Manejo de errores
-        echo 'Error de conexión: ' . $e->getMessage();
-    }
-}
-
+        private function __construct(){
+        try {
+            // Información de conexión
+            // Cadena de conexión
+            $dsn = "pgsql:host=$this->host;port=$this->port;dbname=$this->database;user=$this->user;password=$this->password";
+        
+            // Crear una instancia de PDO
+            self::$db = new PDO($dsn);
+        
+            // Configurar el modo de error de PDO para manejar excepciones
+            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            // Puedes usar esta conexión para realizar consultas
+        } catch (PDOException $e) {
+            // Manejo de errores
+            echo 'Error de conexión: ' . $e->getMessage();
+        }
+        }
+        
     public static function getInstance(){
         if (isset (self::$db))
             return self::$db;

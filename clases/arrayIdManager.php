d98fc2e7ae2c7cc9112395520337d19594278bef
acvidela <acvidela@gmail.com>
2023-11-08 20:40:07 -0300

    revisada y comentada

--- clases/arrayIdManager.php
+++ clases/arrayIdManager.php
@@ -3,8 +3,11 @@ abstract class ArrayIdManager {
     protected $arreglo = [];
     
 
-    protected function getArreglo(){
-        return ($this->arreglo);
+    /**
+     * Get the value of arreglo
+     */ 
+    public function getArreglo() {
+        return $this->arreglo;
     }
 
     // Agregar un objeto nuevo en la posición id del elemento
@@ -25,46 +28,24 @@ abstract class ArrayIdManager {
 
     // Eliminar un elemento por su ID
     public function eliminarPorId($id) {
-        foreach ($this->arreglo as $key => $elemento) {
-            if ($elemento->getId() == $id) {
-                unset($this->arreglo[$key]);
-                break;
-            }
+        if (isset($this->arreglo[$id])){
+            unset($this->arreglo[$id]);
         }
     }
 
 	// Retorna por id el elemento, retorna NULL si no está
     public function getPorId($id) {
-        foreach ($this->arreglo as $elemento) {
-            if ($elemento->getId() == $id) {
-                return $elemento;                
-            }
+        if (isset($this->arreglo[$id])){
+            return $this->arreglo[$id];                
         }
         return NULL;
     }
     
     //Va a modificar recibiendo un objeto, el id permanece
     public function modificar($elementoModificado) {
-      $id = $elementoModificado->getId();
-      if (self::existeId($id)){
-      	foreach ($this->arreglo as $key => $elemento) {
-            if ($elemento->getId() == $id) {
-                $this->arreglo[$key] = $elementoModificado;
-                break;
-            }
-         }
-      }
+        $id = $elementoModificado->getId();
+        if (isset($this->arreglo[$id])){
+            $this->arreglo[$id] = $elementoModificado;
+        }
     } 
-        
-    /**
-     * Set the value of arreglo
-     *
-     * @return  self
-     */ 
-    public function setArreglo($arreglo)
-    {
-        $this->arreglo = $arreglo;
-
-        return $this;
-    }
 }
